6d42052b32d666ea7177c79b90a5d527a5e6ec5d
RosaGestionDigital <rosamaria.duran.contractor@bbva.es>
2016-01-21 12:38:43 +0100

    Enunciado y test Kata SOLID con TDD S 22/01/2016

---
+++ 001_SOLID_S/Inicio/src/com/bbva/SOLID_TDD/SOLID_TDD_S.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class SOLID_TDD_S {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	String CODIGO =
+			"#Programa que pide dos numeros al usuario y los suma\\n"
+			+"LABEL pideNumeroUno\\n"
+			+"PRINT Escribe el primer número:\\n"
+			+"REQ numeroUno\\n"
+			+"GOTOIF pideNumeroUno numeroUno = NULL\\n"
+			+"END_LABEL\\n"
+
+			+"LABEL pideNumeroDos\\n"
+			+"PRINT Escribe el segundo número:\\n"
+			+"REQ numeroDos\\n"
+			+"GOTOIF pideNumeroDos numeroDos = NULL\\n"
+			+"END_LABEL\\n"
+
+
+			+"GOTOIF pideNumeroUno numeroUno = NULL\\n"
+			+"GOTOIF pideNumeroDos numeroDos = NULL\\n"
+
+
+			+"ADD suma numeroUno numeroDos\\n"
+			+"PRINT El resultado es \\n"
+			+"PRINT suma	\\n";
+
+}
