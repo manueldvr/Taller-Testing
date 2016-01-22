a90fd9d8aae9713f096a6d3b6d8068b9329d17d6
Eduardo <milgrammed@gmail.com>
2016-01-22 06:56:58 +0100

    Opción simple

--- 002_SOLID_S_Iterprete/Inicio/SOLID_TDD_S.java
+++ 002_SOLID_S_Iterprete/Inicio/SOLID_TDD_S.java
@@ -12,26 +12,26 @@ public class SOLID_TDD_S {
 	
 	String CODIGO =
 			
-			"#Programa que pide dos numeros al usuario y los suma\\n"
-			+"LABEL pideNumeroUno\\n"
-			+"PRINT Escribe el primer número:\\n"
-			+"REQ numeroUno\\n"
-			+"GOTOIF pideNumeroUno numeroUno = NULL\\n"
-			+"END_LABEL\\n"
+			"#Programa que pide dos numeros al usuario y los suma\n"
+			+"LABEL pideNumeroUno\n"
+			+"PRINT Escribe el primer número:\n"
+			+"REQ numeroUno\n"
+			+"GOTOIF pideNumeroUno numeroUno = NULL\n"
+			+"END_LABEL\n"
 
-			+"LABEL pideNumeroDos\\n"
-			+"PRINT Escribe el segundo número:\\n"
-			+"REQ numeroDos\\n"
-			+"GOTOIF pideNumeroDos numeroDos = NULL\\n"
-			+"END_LABEL\\n"
-
-
-			+"GOTOIF pideNumeroUno numeroUno = NULL\\n"
-			+"GOTOIF pideNumeroDos numeroDos = NULL\\n"
+			+"LABEL pideNumeroDos\n"
+			+"PRINT Escribe el segundo número:\n"
+			+"REQ numeroDos\n"
+			+"GOTOIF pideNumeroDos numeroDos = NULL\n"
+			+"END_LABEL\n"
 
 
-			+"ADD suma numeroUno numeroDos\\n"
-			+"PRINT El resultado es \\n"
-			+"PRINT suma	\\n";
+			+"GOTOIF pideNumeroUno numeroUno = NULL\n"
+			+"GOTOIF pideNumeroDos numeroDos = NULL\n"
+
+
+			+"ADD suma numeroUno numeroDos\n"
+			+"PRINT El resultado es \n"
+			+"PRINT suma\n";
 
 }
