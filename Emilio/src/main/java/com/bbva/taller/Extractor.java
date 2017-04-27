d2f8c546c34cd19f72c85d2fb3352aec55aad548
ejalarcon <ejalarcon@gmail.com>
2017-04-27 16:53:12 +0200

    Proyecto testing calculadora

---
+++ Emilio/src/main/java/com/bbva/taller/Extractor.java
@@ -0,0 +1,14 @@
+package com.bbva.taller;
+
+public class Extractor {
+
+    public int[] extraerOperandos(String cadena) {
+        int [] operandos = {Integer.parseInt(cadena.charAt(0)+""), Integer.parseInt(cadena.charAt(2)+"")};
+        return operandos;
+    }
+
+    public String extraerOperador(String cadena) {
+        return String.valueOf(cadena.charAt(1));
+    }
+
+}
